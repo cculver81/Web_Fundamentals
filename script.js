c3ce34429c8ac838c141b7676f00ec3b09d6d448
Cinque Culver <culver.ce@gmail.com>
2020-02-17 17:10:42 -0500

    initial coding and repository setup

---
+++ script.js
@@ -0,0 +1,2 @@
+//The is practice code in conjuctions with practicing version control with Git.
+//console.log("I started Coding Dojo today. I am excited. I have been struggling for strength all day but I feel that I am already more self-sufficient.")
