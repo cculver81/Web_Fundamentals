731f8e57498995803781143c363e3d13408631f2
Cinque Culver <culver.ce@gmail.com>
2020-02-17 22:08:25 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //The is practice code in conjuctions with practicing version control with Git.
-//console.log("I started Coding Dojo today. I am excited. I have been struggling for strength all day but I feel that I am already more self-sufficient.")
+console.log("I started Coding Dojo today. I am excited. I have been struggling for strength all day but I feel that I am already more self-sufficient.")
+console.log("Now I am already writing basic javascript functions and playing around with Git repositories.")
+
+var x = 3
+for (i = 0; i <= x, i++){
+    console.log (i)
+}
